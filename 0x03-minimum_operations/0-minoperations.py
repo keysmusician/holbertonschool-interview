916419f12d4361f1644c9acaae5c4cb210d3192e
Justin Masayda <justin.masayda@gmail.com>
2022-01-06 23:29:08 -0500

    +x

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python3
-from math import gcd
+"""
+In a text file, there is a single character H. Your text editor can execute
+only two operations in this file: Copy All and Paste. Given a number n, write a
+method that calculates the fewest number of operations needed to result in
+exactly n H characters in the file.
+"""
 
+
+def minOperations(n):
+    """
+    Computes the answer to the Minimum Operations problem.
+    """
+    minimum_operations = 0
+    divisor = 2
+    while n > 1:
+        while n % divisor == 0:
+            minimum_operations += divisor
+            n /= divisor
+        divisor += 1
+    return minimum_operations
