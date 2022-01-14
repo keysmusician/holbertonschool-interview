bc148c6b248b0d07f17dd83872f6f92e86fd6f49
Justin Masayda <justin.masayda@gmail.com>
2022-01-13 20:22:34 -0500

    Start interview question

---
+++ 0x04-sandpiles/0-sandpiles.c
@@ -0,0 +1,41 @@
+/**
+ * print_grid - Print 3x3 grid
+ * @grid: 3x3 grid
+ *
+ */
+static void print_grid(int grid[3][3])
+{
+    int i, j;
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (j)
+                printf(" ");
+            printf("%d", grid[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+/**
+ * sandpiles_sum - computes the sum of two sandpiles
+ *
+ * @grid1: Left 3x3 grid
+ * @grid2: Right 3x3 grid
+ */
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+	int i = 0, j = 0;
+
+	for (;i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grid1[i][j] += grid2[i][j];
+		}
+	}
+
+	print_grid(grid1);
+}
