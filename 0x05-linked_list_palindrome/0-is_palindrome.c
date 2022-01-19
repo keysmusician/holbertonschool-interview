5ed2f28a72732318707ccc88beccddeadad1ab8b
Justin Masayda <justin.masayda@gmail.com>
2022-01-18 20:50:47 -0500

    Add task 0

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+/* DELETE */
+#include <stdio.h>
+
+/**
+ * is_palindrome - Determines if a singly linked list is a palindrome.
+ * @head: the head node of the linked list
+ * Return: 1 if the linked list is a palidrome, 0 if not.
+ */
+int is_palindrome(listint_t **head)
+{
+    int length = 0, index = 0, *compare = NULL;
+    listint_t *node = NULL;
+
+    if (!head || !*head)
+        return 0;
+    node = *head;
+
+    /* Find the length of the LL */
+    while (node) {
+        length++;
+        node = node->next;
+    }
+
+    /* Remember the first half of the LL in reverse order */
+    compare = (int *)calloc(length / 2, sizeof(int));
+    index = length / 2;
+    for (node = *head; node && index; node = node->next) {
+        compare[index - 1] = node->n;
+        index--;
+    }
+
+    if (length % 2 == 1 )
+        node = node->next;
+
+    /* Compare the array with the LL */
+    for (index = 0; index < length && node; index++) {
+        printf("is_palindrome: comparing node->n:'%d' compare[%d]: %d\n", node->n, index, compare[index]);
+        if (node->n != compare[index])
+            return 0;
+        node = node->next;
+    }
+
+    return (1);
+}
