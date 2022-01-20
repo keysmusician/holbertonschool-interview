9449216cf87dcd64f810fd8dda5228d2291412f2
Justin Masayda <justin.masayda@gmail.com>
2022-01-19 23:47:44 -0500

    Recursive implementation

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,46 +1,47 @@
 #include "lists.h"
 
 /**
+ * recursive_check - helper for is_palindrome
+ *
+ * @A: initially the head node, but is recursively descended and used as a
+ * backwards iterator
+ * @head: the head node of the linked list, remains constant through recursion
+ * @result: an output parameter containing the result; must be 1 when passed in
+ */
+void recursive_check(listint_t *A, listint_t *head, int *result)
+{
+	static int length, index;
+	static listint_t *B;
+
+	if (!A)
+		return;
+
+	B = head;
+	length++;
+	index++;
+	recursive_check(A->next, head, result);
+	index--;
+	if (index >= length / 2)
+	{
+		if (B->n == A->n)
+			B = B->next;
+		else
+			*result = 0;
+	}
+}
+
+/**
  * is_palindrome - Determines if a singly linked list is a palindrome.
  * @head: the head node of the linked list
  * Return: 1 if the linked list is a palidrome, 0 if not.
  */
 int is_palindrome(listint_t **head)
 {
-	int length = 0, index = 0;
-	int compare[1000000];
-	listint_t *node = NULL;
+	int result = 1;
 
 	if (!head || !*head)
 		return (0);
-	node = *head;
-
-	/* Find the length of the LL */
-	while (node)
-	{
-		length++;
-		node = node->next;
-	}
-
-	/* Remember the first half of the LL in reverse order */
-	index = length / 2;
-	for (node = *head; node && index; node = node->next)
-	{
-		compare[index - 1] = node->n;
-		index--;
-	}
-
-	if (length % 2 == 1)
-		node = node->next;
-
-	/* Compare the array with the LL */
-	for (index = 0; index < length && node; index++)
-	{
-		if (node->n != compare[index])
-		{
-			return (0);
-		}
-		node = node->next;
-	}
-	return (1);
+	recursive_check(*head, *head, &result);
+	return (result);
 }
+
