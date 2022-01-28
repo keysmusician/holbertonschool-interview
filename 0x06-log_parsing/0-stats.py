63ff778eb38c5a39408e9f092217345bc1261464
Justin Masayda <justin.masayda@gmail.com>
2022-01-27 20:42:31 -0500

    Try not using Counter

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,22 +1,10 @@
 #!/usr/bin/python3
-from collections import Counter
 from sys import stdin
 
 
 line_number = 1
 total_file_size = 0
-# codes = [200, 301, 400, 401, 403, 404, 405, 500]
-code_counts = Counter()
-
-
-# def handle_sigint(signal, frame):
-#     print('File size:', total_file_size)
-#     for code, count in sorted(code_counts.items()):
-#         print('{}: {}'.format(code, count))
-#     exit()
-
-
-# signal.signal(signal.SIGINT, handle_sigint)
+code_counts = dict()
 
 while True:
     line = stdin.readline()\
@@ -26,7 +14,11 @@ while True:
     try:
         IP_address, date, time, status_code, file_size = line.split()
         total_file_size += int(file_size)
-        code_counts[int(status_code)] += 1
+        if code_counts.get(int(status_code)):
+            code_counts[int(status_code)] += 1
+        else:
+            code_counts[int(status_code)] = 1
+
     except ValueError:
         pass
 
