34feaf43446a7851236e7cfafad0184cddb3634b
Justin Masayda <justin.masayda@gmail.com>
2022-02-17 10:02:37 -0500

    Fix multibyte overlong detection

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -8,6 +8,7 @@ encoding.
 def validUTF8(data):
     """Validates that `data` is valid UTF-8."""
     continuation_bytes = 0
+    start_multibyte = False
     for byte in data:
         check_bit = 0x80
         if byte & check_bit:
@@ -20,18 +21,22 @@ def validUTF8(data):
                     if continuation_bytes > 3:
                         # Too many continuation bytes
                         return False
+                    start_multibyte = True
                     continuation_bytes += 1
                     check_bit >>= 1
                 # Byte might be overlong; Count the free bits
-                free_bits = 8 - (continuation_bytes + 2)
+                free_bits = 6 - continuation_bytes
+                if (check_bit - 1) & byte:
+                    free_bits = 0
                 if continuation_bytes == 0:
                     # An unexpected continuation byte was encountered
                     return False
             else:
                 # A continuation byte is expected
-                if (byte & 0x3F).bit_length() >= free_bits:
+                if start_multibyte and (byte & 0x3F).bit_length() <= free_bits:
                     # Overlong encoding encountered
                     return False
+                start_multibyte = False
                 if check_bit & byte:
                     # A non-continuation byte was encountered
                     return False
