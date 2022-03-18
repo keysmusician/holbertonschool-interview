8c842e2a671b280970ef383a62f0a7650c3c9a7f
Justin Masayda <justin.masayda@gmail.com>
2022-03-17 21:11:27 -0400

    Update solution

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -17,28 +17,56 @@ except ValueError:
     print("N must be a number")
     exit(1)
 
+
+class GridSquare:
+    board_size = N
+
+    def __init__(self, row, column):
+        if row < 0 or column < 0:
+            raise ValueError
+        self.row = row
+        self.column = column
+
+    def __repr__(self):
+        return str([self.row, self.column])
+
+    def index(self):
+        return self.row * self.board_size + self.column
+
+    def diagonal_forward(self):
+        return self.row + self.column
+
+    def diagonal_backward(self):
+        return self.board_size - 1 - (self.row - self.column)
+
+
 # Algorithm:
-n_queens = []
-n_queens2 = []
-n_queens3 = []
-n_queens4 = []
-row = 1
-for col in range(N):
-    if row >= N:
-        row = 0
-    n_queens.append((col, row))
-    n_queens2.append((row, N - 1 - col))
-    n_queens3.append((row, col))
-    n_queens4.append((col, N - 1 - row))
-    row += 2
+n_queens = [n for n in range(N)]
 
-# Remove duplicates
-results = set()
-[
-    results.add(tuple(sorted(l)))
-    for l in (n_queens, n_queens2, n_queens3, n_queens4)
-]
 
-# Print results
-for result in results:
-    print(str(result).replace('(', '[').replace(')', ']'))
+def permutations(elements):
+    if len(elements) <= 1:
+        yield elements
+    else:
+        for permutation in permutations(elements[1:]):
+            for i in range(len(elements)):
+                yield permutation[:i] + elements[0:1] + permutation[i:]
+
+
+for permutation in permutations(n_queens):
+    solution = True
+    permutation = [GridSquare(i, n) for i, n in enumerate(permutation)]
+
+    forward_diagonals = []
+    backward_diagonals = []
+    for square in permutation:
+        fd = square.diagonal_forward()
+        bd = square.diagonal_backward()
+        if fd in forward_diagonals or bd in backward_diagonals:
+            solution = False
+            break
+        forward_diagonals.append(fd)
+        backward_diagonals.append(bd)
+
+    if solution:
+        print(permutation)
