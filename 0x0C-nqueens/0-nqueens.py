55883f5dd6867034f9edaf0490cc60e111699573
Justin Masayda <justin.masayda@gmail.com>
2022-03-17 21:20:48 -0400

    Simplify solution

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -17,30 +17,8 @@ except ValueError:
     print("N must be a number")
     exit(1)
 
-
-class GridSquare:
-    board_size = N
-
-    def __init__(self, row, column):
-        if row < 0 or column < 0:
-            raise ValueError
-        self.row = row
-        self.column = column
-
-    def __repr__(self):
-        return str([self.row, self.column])
-
-    def index(self):
-        return self.row * self.board_size + self.column
-
-    def diagonal_forward(self):
-        return self.row + self.column
-
-    def diagonal_backward(self):
-        return self.board_size - 1 - (self.row - self.column)
-
-
-# Algorithm:
+# Algorithm: generate all permutations of columns containing a single queen,
+# and filter out solutions which contain conflicts along diagonals
 n_queens = [n for n in range(N)]
 
 
@@ -55,13 +33,11 @@ def permutations(elements):
 
 for permutation in permutations(n_queens):
     solution = True
-    permutation = [GridSquare(i, n) for i, n in enumerate(permutation)]
-
     forward_diagonals = []
     backward_diagonals = []
-    for square in permutation:
-        fd = square.diagonal_forward()
-        bd = square.diagonal_backward()
+    for row, column in enumerate(permutation):
+        fd = row + column
+        bd = N - 1 - (row - column)
         if fd in forward_diagonals or bd in backward_diagonals:
             solution = False
             break
@@ -69,4 +45,4 @@ for permutation in permutations(n_queens):
         backward_diagonals.append(bd)
 
     if solution:
-        print(permutation)
+        print([[i, n] for i, n in enumerate(permutation)])
