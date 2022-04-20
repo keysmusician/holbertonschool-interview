559601709c274de913090720aefeb67738101693
Justin Masayda <justin.masayda@gmail.com>
2022-04-20 18:32:47 -0400

    Update task 0

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -1,6 +1,34 @@
 #include "sort.h"
 
 /**
+ * heapify - Convert an array to a max heap
+ * @array: Array to convert
+ * @size: Size of the array
+ * @index: Heap starting index
+ * @heap_size: Size of the heap
+ */
+void heapify(int *array, size_t size, int index, size_t heap_size)
+{
+	int largest = index;
+	int left_child = 2 * index + 1;
+	int right_child = 2 * index + 2;
+	int temp;
+
+	if (left_child < (int)size && array[left_child] > array[largest])
+		largest = left_child;
+	if (right_child < (int)size && array[right_child] > array[largest])
+		largest = right_child;
+	if (largest != index)
+	{
+		temp = array[index];
+		array[index] = array[largest];
+		array[largest] = temp;
+		print_array(array, heap_size);
+		heapify(array, size, largest, heap_size);
+	}
+}
+
+/**
  * heap_sort - Sort an array of integers in ascending order using the Heap Sort
  * algorithm
  *
@@ -9,39 +37,16 @@
  */
 void heap_sort(int *array, size_t size)
 {
-	size_t left_child, right_child, heap_size = size;
-	int temp, heap_index, largest;
+	int index, temp;
 
-	while (heap_size > 0)
+	for (index = (int)size / 2 - 1; index >= 0; index--)
+		heapify(array, size, index, size);
+	for (index = (int)size - 1; index > 0; index--)
 	{
-		heap_index = heap_size / 2 - 1;
-		/* Convert the array to a max heap */
-		for (; heap_index >= 0; heap_index--)
-		{
-			largest = heap_index;
-			left_child = heap_index * 2 + 1;
-			if (left_child < heap_size && array[left_child] > array[largest])
-				largest = left_child;
-			right_child = heap_index * 2 + 2;
-			if (right_child < heap_size && array[right_child] > array[largest])
-				largest = right_child;
-
-			if (largest != heap_index)
-			{
-				temp = array[heap_index];
-				array[heap_index] = array[largest];
-				array[largest] = temp;
-				print_array(array, size);
-			}
-		}
-		/* Swap the root with the end of the array, and decrement heap size */
-		if (array[0] != array[heap_size - 1])
-		{
-			temp = array[0];
-			array[0] = array[heap_size - 1];
-			array[heap_size - 1] = temp;
-			print_array(array, size);
-		}
-		--heap_size;
+		temp = array[0];
+		array[0] = array[index];
+		array[index] = temp;
+		print_array(array, size);
+		heapify(array, (size_t)index, 0, size);
 	}
 }
