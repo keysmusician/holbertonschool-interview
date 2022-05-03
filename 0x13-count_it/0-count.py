090f79fa772c2725a858fd62faee7eb07696ae4f
Justin Masayda <justin.masayda@gmail.com>
2022-05-03 16:41:27 -0400

    Add task 0

---
+++ 0x13-count_it/0-count.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Write a recursive function that queries the Reddit API, parses the title of all
+hot articles, and prints a sorted count of given keywords.
+"""
+from collections import Counter
+import requests
+
+
+def count_words(subreddit, word_list):
+    """
+    Prints a sorted count of given keywords found in Reddit's hot articles.
+    """
+    word_count = Counter()
+
+    def count_recursion(after=None):
+        nonlocal word_count
+        url = f'http://www.reddit.com/r/{subreddit}/hot.json'
+        headers = {'User-Agent': '0x13 Count It!'}
+        parameters = {
+            'after': after,
+            'limit': 100,
+            'show': 'all'
+            }
+        response = requests.get(url, headers=headers, params=parameters)
+        if not response.ok:
+            return
+        response = response.json()
+
+        after = response['data']['after']
+        for child in response['data']['children']:
+            title = child['data']['title'].lower().split()
+            for word in title:
+                for search_word in word_list:
+                    if word == search_word.lower():
+                        word_count[word] += 1
+        if after:
+            count_recursion(after)
+
+    count_recursion()
+
+    sorted_count = sorted(word_count.items(), key=lambda x:(-x[1], x[0]))
+    for key, value in sorted_count:
+        print(f'{key}: {value}')
---
+++ 0x13-count_it/README.md
@@ -0,0 +1,32 @@
+<p align="center">
+  <a href=#>
+    <img src="https://intranet.hbtn.io/assets/holberton-logo-full-black-157ccfa3d2134776c1e3f78c0fe682968e8848b64fcacc6187976044f75f35a8.png" alt="Holberton School logo">
+  </a>
+</p>
+
+# 0x13. Count it!
+
+## Table of Contents
+* [Prompt](#prompt)
+* [Files](#files)
+* [Author](#author)
+
+## Prompt
+
+Write a recursive function that queries the Reddit API, parses the title of all hot articles, and prints a sorted count of given keywords.
+
+## Files
+* `0-count.py`
+* `README.md`
+
+## Author
+Justin Masayda [@keysmusician](https://github.com/keysmusician)
+<pre align="center">
+       _   _       _   _   _       _   _       _   _   _
+      ___//|_//|_____//|_//|_//|_____//|_//|_____//|_//|_//|___
+     /  /// ///  /  /// /// ///  /  /// ///  /  /// /// ///  / |
+   /  ||/ ||/  /  ||/ ||/ ||/  /  ||/ ||/  /  ||/ ||/ ||/  / /
+ /___/___/___/___/___/___/___/___/___/___/___/___/___/___/ /
+|___|___|___|___|___|___|___|___|___|___|___|___|___|___|/
+</pre>
+<p><span style="font-family: 'Lucida Console'; line-height: 14px; font-size: 14px; display: inline-block;">&nbsp;</span></p>
