9d178745a67df971e8f1be91196fb7e4489ec5d1
keysmusician <justin.masayda@gmail.com>
2022-05-14 03:57:03 +0000

    Update solution

--- 0x14-heap_extract/binary_trees.h
+++ 0x14-heap_extract/binary_trees.h
@@ -1,5 +1,8 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
+
+#include <stdlib.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -17,6 +20,8 @@ struct binary_tree_s
 };
 typedef struct binary_tree_s heap_t;
 typedef struct binary_tree_s binary_tree_t;
+
 void binary_tree_print(const binary_tree_t *);
 int heap_extract(heap_t **root);
+
 #endif /* BINARY_TREES_H */
