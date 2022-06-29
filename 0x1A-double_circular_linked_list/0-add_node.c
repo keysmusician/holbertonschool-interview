2ea79456e853553516f7f1977302c5d03ced3397
Justin Masayda <justin.masayda@gmail.com>
2022-06-29 16:22:21 -0400

    Add task 0

---
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -0,0 +1,155 @@
+#include "list.h"
+
+/**
+ * add_node_end - Adds a node to the end of a circular doubly linked list
+ *
+ * @list: a list to add a node to the end of
+ * @str: string content of the node
+ * Return: pointer to the new node
+ */
+List *add_node_end(List **list, char *str)
+{
+	List *temp, *new_node = NULL;
+	char *str_copy;
+
+	if (!list || !str)
+		return (NULL);
+
+	/* Initialize a new node: */
+	new_node = (List *)malloc(sizeof(List));
+	str_copy = strdup(str);
+	if (!new_node || !str_copy)
+	/* Memory allocation failed */
+		return (NULL);
+	new_node->str = str_copy;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+
+	if (!*list)
+	/* There is a list, but no head node: */
+		*list = new_node;
+	else
+	/* There is a head node */
+	{
+		if (!(*list)->prev)
+		/* ...but no previous node */
+		{
+			if (!(*list)->next)
+			/* ...and no next node */
+				{
+					/* The head node is the only node; add the new node */
+					(*list)->prev = new_node;
+					(*list)->next = new_node;
+					new_node->prev = *list;
+					new_node->next = *list;
+				}
+			else
+			/* There is a next node; The list is not circular */
+			{
+				free(new_node->str);
+				free(new_node);
+				return (NULL);
+			}
+		}
+		else
+		/* There is a previous node */
+		{
+			if (!(*list)->next)
+			/* ...but no next node; The list is not circular */
+			{
+				free(new_node->str);
+				free(new_node);
+				return (NULL);
+			}
+			else
+			/* There is a next and previous node */
+			{
+				temp = (*list)->prev;
+				(*list)->prev = new_node;
+				new_node->next = *list;
+				new_node->prev = temp;
+				temp->next = new_node;
+			}
+		}
+	}
+
+	return (new_node);
+}
+
+/**
+ * add_node_begin - Adds a node to the beginning of a circular doubly linked
+ * list
+ *
+ * @list: a list to add a node to the beginning of
+ * @str: string content of the node
+ * Return: pointer to the new node
+ */
+List *add_node_begin(List **list, char *str)
+{
+	List *temp, *new_node = NULL;
+	char *str_copy;
+
+	if (!list || !str)
+	/* There is no list or no string */
+		return (NULL);
+
+	/* Initialize a new node: */
+	new_node = (List *)malloc(sizeof(List));
+	str_copy = strdup(str);
+	if (!new_node || !str_copy)
+	/* Memory allocation failed */
+		return (NULL);
+	new_node->str = str_copy;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+
+	if (!*list)
+	/* There is a list, but no head node: */
+		*list = new_node;
+	else
+	/* There is a head node */
+	{
+		if (!(*list)->prev)
+		/* ...but no previous node */
+		{
+			if (!(*list)->next)
+			/* ...and no next node */
+				{
+					/* The head node is the only node; add the new node */
+					(*list)->prev = new_node;
+					(*list)->next = new_node;
+					new_node->prev = *list;
+					new_node->next = *list;
+				}
+			else
+			/* There is a next node; The list is not circular */
+			{
+				free(new_node->str);
+				free(new_node);
+				return (NULL);
+			}
+		}
+		else
+		/* There is a previous node */
+		{
+			if (!(*list)->next)
+			/* ...but no next node; The list is not circular */
+			{
+				free(new_node->str);
+				free(new_node);
+				return (NULL);
+			}
+			else
+			/* There is a next and previous node */
+			{
+				temp = (*list)->next;
+				(*list)->next = new_node;
+				new_node->prev = *list;
+				new_node->next = temp;
+				temp->prev = new_node;
+			}
+		}
+	}
+
+	return (new_node);
+}
---
+++ 0x1A-double_circular_linked_list/README.md
@@ -0,0 +1,56 @@
+<p align="center">
+  <a href=#>
+    <img src="https://user-images.githubusercontent.com/74752740/175812508-dc2482bf-bd5b-4c0a-b075-1bede95c488e.png" alt="Holberton School logo">
+  </a>
+</p>
+
+# 0x1A. Double Circular Linked List
+
+## Table of Contents
+* [Prompt](#prompt)
+* [Files](#files)
+* [Author](#author)
+
+## Prompt
+Create the source file `0-add_node.c` that contains the following functions:
+
+Add a new node to the end of a double circular linked list:
+
+Prototype: `List *add_node_end(List **list, char *str);`
+
+List: the list to modify
+
+str: the string to copy into the new node
+
+Returns: Address of the new node, or NULL on failure
+
+
+Add a new node to the beginning of a double circular linked list:
+
+Prototype: `List *add_node_begin(List **list, char *str);`
+
+List: the list to modify
+
+str: the string to copy into the new node
+
+Returns: Address of the new node, or NULL on failure
+
+* You are not allowed to use global variables
+* No more than 5 functions per file
+* The only C standard library functions allowed are `malloc`, `free`, and `strdup`
+
+## Files
+* `0-making_change.py`
+* `README.md`
+
+## Author
+Justin Masayda [@keysmusician](https://github.com/keysmusician)
+<pre align="center">
+        _   _       _   _   _       _   _       _   _   _     
+    ___//|_//|_____//|_//|_//|_____//|_//|_____//|_//|_//|___ 
+   /  /// ///  /  /// /// ///  /  /// ///  /  /// /// ///  / |
+  /  ||/ ||/  /  ||/ ||/ ||/  /  ||/ ||/  /  ||/ ||/ ||/  / / 
+ /___/___/___/___/___/___/___/___/___/___/___/___/___/___/ /  
+ |___|___|___|___|___|___|___|___|___|___|___|___|___|___|/   
+
+</pre>
