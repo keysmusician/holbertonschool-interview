c1f04713a883a3e3f4fc144af07dfa6f88d18a70
Justin Masayda <justin.masayda@gmail.com>
2022-07-07 18:22:04 -0400

    Use more elegant syntax; Add comments

--- 0x1B-radix_sort/0-radix_sort.c
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -14,44 +14,43 @@
  */
 void radix_sort(int *array, size_t size)
 {
-	long digit_place = 1;
-	int histogram[DIGIT_COUNT] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	int histogram[DIGIT_COUNT], digit, digit_place, *array_copy = NULL;
+	char increase_digit_place;
 	size_t i;
-	int digit, *array_copy = NULL;
-	char increase_digit_place = TRUE;
 
 	array_copy = (int *)malloc(sizeof(int) * size);
 	if (!array_copy)
 		return;
 
-	while (increase_digit_place)
+	for (digit_place = 1; ; digit_place *= DIGIT_COUNT)
 	{
-		increase_digit_place = FALSE;
+		increase_digit_place = FALSE; /* Assume this will be our final loop */
+
+		for (i = 0; i < DIGIT_COUNT; i++) /* Clear the histogram */
+			histogram[i] = 0;
+
 		for (i = 0; i < size; i++)
 		{
 			array_copy[i] = array[i]; /* Copy the array */
-			if (array[i] / digit_place > 0)
+			if (array[i] / digit_place > 0) /* Check if we need to loop */
 				increase_digit_place = TRUE;
 			digit = array[i] / digit_place % DIGIT_COUNT;
 			histogram[digit]++; /* Count occurrences of each digit */
 		}
 		if (!increase_digit_place)
-			break; /* If all digits less than digit_place, we're done */
+			break; /* If all digits are less than digit_place, we're done */
+
 		for (i = 1; i < DIGIT_COUNT; i++)
 			histogram[i] += histogram[i - 1]; /* Convert counts to indicies */
 
-		for (i = size - 1; ; i--)
-		{
+		i = size - 1;
+		do { /* Sort the array by digit */
 			digit = array_copy[i] / digit_place % DIGIT_COUNT;
 			histogram[digit]--;
 			array[histogram[digit]] = array_copy[i];
-			if (i == 0)
-				break;
-		}
+		} while (i-- > 0);
+
 		print_array(array, size);
-		digit_place *= DIGIT_COUNT;
-		for (i = 0; i < DIGIT_COUNT; i++) /* Reset histogram */
-			histogram[i] = 0;
 	}
 	free(array_copy);
 }
