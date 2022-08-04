dd791fbd5b0e2515d500201ad9bf9f4fafd72168
Justin Masayda <justin.masayda@gmail.com>
2022-08-04 16:11:13 -0400

    Fix case when sums exceed one digit

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -10,4 +10,17 @@ def pascal_triangle(n):
     Returns a list of lists of integers representing the Pascal's triangle of
     `n`.
     """
-    return [list(str(11 ** m)) for m in range(n)]
+    result = []
+    previous_row = [1]
+
+    for _ in range(n):
+        result.append(previous_row)
+        next_row = [
+            a + b for a, b in zip([0] + previous_row, previous_row + [0])
+        ]
+        # Copying `previous_row` twice isn't very space efficient, but it
+        # is readable. If performance is an issue, indexing the previous row
+        # directly should improve it.
+        previous_row = next_row
+
+    return result
